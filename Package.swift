cb3c3c9708e79ee70cb40732e7374c7ca52c89d3
phoenix <x.rhythm@qq.com>
2025-08-22 23:05:05 +0800

    Update YYImage version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Asura19/YYImage.git", from: "1.0.5"),
+        .package(url: "https://github.com/Asura19/YYImage.git", from: "1.0.6"),
         .package(url: "https://github.com/Asura19/YYCache.git", from: "1.0.5"),
     ],
     targets: [
